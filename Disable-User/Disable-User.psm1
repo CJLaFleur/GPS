ee5cde17747be729ac95ed6a0f55ad5cb436ef58
CJ LaFleur <lafleurc@wit.edu>
2022-07-21 14:49:41 -0400

    Fixed the algorithm to determine user site, added ability to read from csv.

--- Disable-User/Disable-User.psm1
+++ Disable-User/Disable-User.psm1
@@ -4,27 +4,46 @@
  [CmdletBinding()]
   param(
 
-    [Parameter(Mandatory = $True,
+    [Parameter(Mandatory = $False,
       Position = 0,
       HelpMessage = "Specify the users you want to disable.")]
-    [String[]]$Users
+    [String[]]$Users,
+
+    [Parameter(Mandatory = $False,
+      HelpMessage = "Specify the primary site for the users.")]
+      [Alias('Input', 'In', 'if')]
+      $InputFile
 
   )
 
 
+  if ($InputFile){
 
+        $FileReader = New-Object System.IO.StreamReader -Arg $InputFile
+
+        while ($FileReader.EndOfStream -EQ $False) {
+            
+            $Users += $FileReader.ReadLine()
+            
+            }
+             
+             $FileReader.Dispose()
+             
+             $FileReader.Close()
+        }
+        
   foreach ($U in $Users) {
 
     $UserDN  = (Get-ADUser -Identity $U).distinguishedName
     $UserDNStr = $UserDN.ToString()
-    $Site = ($UserDNStr -split ",")[4].substring(3)
+    $Temp = (Get-ADUser -Identity $U -Properties HomeDirectory).HomeDirectory
+    $Site = $Temp.Substring(2,3)
     $GHSStaffOU = "OU=Disabled Staff Accounts,OU=Staff,OU=Users,OU=" + "$Site" + ",OU=District Sites,OU=GPSK12_TREE,DC=GPSK12,DC=local"
     $StudentOU = "OU=Disabled,OU=Students,OU=Users,OU=" + "$Site" + ",OU=District Sites,OU=GPSK12_TREE,DC=GPSK12,DC=local"
     $GMSStudentOU = "OU=Disabled - Left District,OU=Students,OU=Users,OU=" + "$Site" + ",OU=District Sites,OU=GPSK12_TREE,DC=GPSK12,DC=local"
-    $GHSStaffOU = "OU=Disabled Staff Accounts,OU=Staff,OU=Users,OU=" + "$Site" + ",OU=District Sites,OU=GPSK12_TREE,DC=GPSK12,DC=local"
     $NPSStaffOU = "OU=Disabled Accounts,OU=Staff,OU=Users,OU=" + "$Site" + ",OU=District Sites,OU=GPSK12_TREE,DC=GPSK12,DC=local"
     
-    
+    Set-ADUser -Identity $UserDN -Enabled $False
     
     if ($UserDNStr -match "Student" -and $Site -match "GMS") {
 
@@ -61,8 +80,5 @@
      Write-Host "Error"
 
      }
-
-     Set-ADUser -Identity $UserDN -Enabled $False
   }
-
 }
